ef3c6553f11c508f75664660df2f8dcc58d9a8ed
AlexandraFernandez <alemoralesfernandez@crackthecode.la>
2025-09-08 22:03:09 -0500

    Actualizado

--- section33/assets/js/script.js
+++ section33/assets/js/script.js
@@ -21,7 +21,14 @@ console.log(subtitles[0].innerText);
 console.log(subtitles[1].innerText); 
 console.log(subtitles[2].innerText); 
 
+
 //selector por css
+/*
+Selectores por CSS, se llaman asi porque usan la sintaxis de un selector CSS para seleccionar los elementos del DOM.
+
+querySelector("selector") -> Devuelve el primer elemento que coincida con el selector.
+querySelectorAll("selector") -> Devuelve un arreglo de elementos que coincidan con el selector.
+*/
 
 let queryMainTitle = document.querySelector('#main-title');  //para id
 console.log(queryMainTitle);
@@ -29,21 +36,80 @@ console.log(queryMainTitle);
 let queryMainTexts = document.querySelectorAll(".main-text");   //para class
 console.log(queryMainTexts);
 
+
+
 //EVENTOS
 document.getElementById("main-button").addEventListener("click", 
     function (){
         console.log("Diste click en el boton");
-});
-/*Podemos cambiar estilos*/
-let currentBackgroundColor = document.body.style.backgroundColor;
+        /*Podemos cambiar estilos*/
+        let currentBackgroundColor = document.body.style.backgroundColor;
 
-if (currentBackgroundColor === "red") {
-    document.body.style.backgroundColor = "white";
-    mainButton.style.color = "white";
-} else {
-    document.body.style.backgroundColor = "red";
-    mainButton.style.color = "red";
-}
-let magicNumber = document.querySelector("#magic-number");
+        if (currentBackgroundColor === "red") {
+            document.body.style.backgroundColor = "white";
+            mainButton.style.color = "white";
+        } else {
+            document.body.style.backgroundColor = "red";
+            mainButton.style.color = "red";
+        }
+        let magicNumber = document.querySelector("#magic-number").value;
+        console.log(`Valor ingresado en el input: ${magicNumber}`);
+
+        // Mostramos el valor en el contenido de un elemento
+        let greetParagraph = document.querySelector("#greet");
+
+        // Validacion en caso el input esté vacío
+        if (magicNumber == "" || magicNumber.length === 0) {
+            greetParagraph.innerText = "NO INGRESASTE UN NOMBRE";
+        } else {
+            greetParagraph.innerText = `Buenas noches ${magicNumber}`;
+        }
+    }
+);
+
+// Evento de click en otro boton
+// Selecionamos el boton
+let togglerButton = document.querySelector("#toggler");
+// Fijamos el contenido
+togglerButton.innerText = "MOSTRAR";
+// Seleccionamos el texto
+let hiddenText = document.querySelector("#hidden-text");
+// Lo ocultamos directamente
+hiddenText.style.display = "none";
+
+// Agregamos el event listener
+togglerButton.addEventListener("click", function () {
+  let currentTextDisplay = hiddenText.style.display;
+
+  // Si el boton esta oculto
+  if (currentTextDisplay == "none") {
+    // Lo muestro
+    hiddenText.style.display = "block";
+    togglerButton.innerText = "OCULTAR";
+  } else {
+    // Si no esta oculto, lo oculto
+    hiddenText.style.display = "none";
+    togglerButton.innerText = "MOSTRAR";
+  }
+});
+
+// FORMA CORRECTA DE CAMBIAR ESTILOS, USANDO CLASES
+let togglerButton2 = document.querySelector("#toggler-2");
+// Fijamos el contenido
+togglerButton2.innerText = "MOSTRAR";
+// Seleccionamos el texto
+let hiddenText2 = document.querySelector("#hidden-text-2");
+
+// Agregamos el event listener
+togglerButton2.addEventListener("click", function () {
+  if (hiddenText2.classList.contains("hide-text")) {
+    hiddenText2.classList.remove("hide-text");
+    togglerButton2.innerText = "OCULTAR";
+  } else {
+    console.log("ELSE");
+    hiddenText2.classList.add("hide-text");
+    togglerButton2.innerText = "MOSTRAR";
+  }
+});
 
 
